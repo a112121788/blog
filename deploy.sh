ebc8d47574f16f12af95183b80cf083ac4ed8ee1
gaoshuaipeng <gaoshuaipeng@ecloudinfo.com>
2017-01-04 14:25:01 +0800

    add books.md

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,7 @@ cp -Rf ../studio/* public/studio/
 rm -rf public/full_stack_book/*
 cp -Rf ../full_stack_book/_book/* public/full_stack_book/
 
+rm -rf public/react-native-learn/*
+cp -Rf ../react-native-learn/_book/* public/react-native-learn/
 
 hexo d
